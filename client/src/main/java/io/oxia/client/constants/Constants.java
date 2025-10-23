fcded7ba912f39a009bea7cc92b635fbb621b64b
mattisonchao <mattisonchao@gmail.com>
2025-10-23 13:26:21 +0800

    spotelss

--- client/src/main/java/io/oxia/client/constants/Constants.java
+++ client/src/main/java/io/oxia/client/constants/Constants.java
@@ -15,7 +15,6 @@
  */
 package io.oxia.client.constants;
 
-
 public final class Constants {
     // As same as oxia server configuration
     public static final int MAXIMUM_FRAME_SIZE = 256 * 1024 * 1024;
