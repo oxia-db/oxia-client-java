bd554c31353abd85fe395d5bb040aa41075216be
mattisonchao <mattisonchao@gmail.com>
2025-11-11 12:35:00 +0800

    add test

--- client/src/test/java/io/oxia/client/session/SessionTest.java
+++ client/src/test/java/io/oxia/client/session/SessionTest.java
@@ -19,8 +19,7 @@ import static io.oxia.client.OxiaClientBuilderImpl.DefaultNamespace;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.lenient;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 import io.grpc.Server;
 import io.grpc.inprocess.InProcessChannelBuilder;
@@ -35,13 +34,13 @@ import io.oxia.proto.CloseSessionResponse;
 import io.oxia.proto.KeepAliveResponse;
 import io.oxia.proto.OxiaClientGrpc;
 import io.oxia.proto.SessionHeartbeat;
+
 import java.io.IOException;
 import java.time.Duration;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -123,6 +122,28 @@ class SessionTest {
                         mock(SessionNotificationListener.class));
         assertThat(session.getShardId()).isEqualTo(shardId);
         assertThat(session.getSessionId()).isEqualTo(sessionId);
+    }
+
+    @Test
+    public void nonCallbackListener() {
+        final OxiaStubProvider mockProvider = mock(OxiaStubProvider.class);
+        final SessionNotificationListener listener = spy(SessionNotificationListener.class);
+        when(mockProvider.getStubForShard(anyLong())).thenThrow(new IllegalStateException("wrong states"));
+        var session =
+                new Session(
+                        executor,
+                        mockProvider,
+                        config,
+                        shardId,
+                        sessionId,
+                        InstrumentProvider.NOOP,
+                        listener
+                );
+        try {
+            session.close();
+        } catch (Throwable ignore) {
+        }
+        verify(listener, times(1)).onSessionClosed(any());
     }
 
     @Test
